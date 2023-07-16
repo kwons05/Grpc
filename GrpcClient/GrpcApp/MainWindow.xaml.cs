2c7ece9c1afc5447d4c5c8fb5b6a414941039f57
kwons <code.sona@gmail.com>
2023-07-16 21:39:10 +0900

    init gRpc Client

---
+++ GrpcClient/GrpcApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using GrpcModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrpcApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GrpcHandle GrpcHandle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GrpcHandle = new GrpcHandle();
+        }
+
+        private void ReqestButton_Click(object sender, RoutedEventArgs e)
+        {
+            string requestText = this.ReqestTextBox.Text;
+
+            var reply = GrpcHandle.Client.SayHello(new HelloRequest { Name = requestText });
+
+            this.ReplayTextBox.Text = reply.Message;
+        }
+    }
+}
---
+++ GrpcClient/GrpcModule/GrpcHandle.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+
+namespace GrpcModule
+{
+    public class GrpcHandle
+    {
+
+        private string URL = "http://localhost:5263";
+
+        public GrpcChannel Channel          { get; set; }
+        public Greeter.GreeterClient Client { get; set; }
+
+        public GrpcHandle()
+        {
+            Channel = GrpcChannel.ForAddress(URL);
+            Client = new Greeter.GreeterClient(Channel);
+        }
+
+    }
+}
